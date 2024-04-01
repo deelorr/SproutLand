5b2be06df0266109c2cd69026b6a7ae820eb2bb8
deelorr <deelorr@gmail.com>
2024-03-31 18:21:50 -0700

    added overlay

--- settings.py
+++ settings.py
@@ -4,11 +4,11 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 TILE_SIZE = 64
 
-# # overlay positions
-# OVERLAY_POSTITIONS = {
-#     'tool' : (40, SCREEN_HEIGHT - 15),
-#     'seed' : (70, SCREEN_HEIGHT - 5),
-# }
+# overlay positions
+OVERLAY_POSITIONS = {
+    'tool' : (40, SCREEN_HEIGHT - 15),
+    'seed' : (70, SCREEN_HEIGHT - 5),
+}
 
 # PLAYER_TOOL_OFFSET = {
 #     'left' : Vector2(-50, 40),
